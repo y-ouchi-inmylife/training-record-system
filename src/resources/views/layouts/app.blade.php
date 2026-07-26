20218b1ae7c399a24631fc5ac828d712e4187e0a
y-ouchi-inmylife <y-ouchi@inmylife1965.com>
2026-07-26 15:56:35 +0900

    style: トレーナー側ナビバーの背景色を紺に変更

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
 </head>
 <body>
     @auth
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-trainer-nav">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ url('/dashboard') }}">トレーニング記録管理システム</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
